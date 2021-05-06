e102a317cd77026eca653237c042ff00440b006b
Oscar Lundberg <info@oscarlundberg.com>
2021-05-06 22:39:51 +0200

    added upgrade function

--- index.js
+++ index.js
@@ -55,8 +55,9 @@ app.get('/deploy-agent/get/help', (req, res) => {
         <ul>
             <li>/deploy-agent/post/deploy - <small>Deploy a new service</small></li>
             <li>/deploy-agent/post/update - <small>Update an existing service</small></li>
-            <li>/deploy-agent/post/reload - <small>Force a running service to reload (auto updates if specified in the run behaviour)</small></li>
+            <li>/deploy-agent/post/reload - <small>Force a running service to reload</small></li>
             <li>/deploy-agent/post/kill - <small>Kill a running service</small></li>
+            <li>/deploy-agent/post/upgrade - <small>Upgrade the service (Requires "upgrade" property to be specified either in service declaration or in request body)</small></li>
         </ul>
         <h3>DELETE</h3>
         <ul><li>'/deploy-agent/delete/service' - <small>Delete the service* (<b>Alpha</b>: The service declaration and home directory will be kept as a backup, but the service will be unlinked and moved)</small></li>
@@ -90,14 +91,16 @@ app.post('/deploy-agent/post/upgrade', (req, res) => {
         res.status(400).send("Service not found");
     } else {
         let homeDir = path.resolve(__dirname + "/service_home/" + target.name);
+
         try {
-            output = execSync(target.upgrade.replace("$CWD", homeDir));
+            let command = req.body.upgrade || target.upgrade;
+            output = execSync(command.replace("$CWD", homeDir));
         } catch (err) {
             res.status(400).send(err);
         }
     }
     res.status(200).send("OK - " + output);
-})
+});
 
 
 app.post('/deploy-agent/post/deploy', (req, res) => {
--- readme.md
+++ readme.md
@@ -14,8 +14,9 @@
 <ul>
     <li>/deploy-agent/post/deploy - <small>Deploy a new service</small></li>
     <li>/deploy-agent/post/update - <small>Update an existing service</small></li>
-    <li>/deploy-agent/post/reload - <small>Force a running service to reload (auto updates if specified in the run behaviour)</small></li>
+    <li>/deploy-agent/post/reload - <small>Force a running service to reload</small></li>
     <li>/deploy-agent/post/kill - <small>Kill a running service</small></li>
+    <li>/deploy-agent/post/upgrade - <small>Upgrade the service (Requires "upgrade" property to be specified either in service declaration or in request body)</small></li>
 </ul>
 <h3>DELETE</h3>
 <ul><li>'/deploy-agent/delete/service' - <small>Delete the service* (<b>Alpha</b>: The service declaration and home directory will be kept as a backup, but the service will be unlinked and moved)</small></li>

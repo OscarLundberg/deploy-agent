46a2431bb5032f245476512600e4ff1e251fa9af
Oscar Lundberg <info@oscarlundberg.com>
2021-05-09 22:37:42 +0200

    fix service issue2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function userAgent() {
         "uptime": Math.floor((os.uptime() / 60) / 60) + " h",
         "serviceDir": (os.type() === "Darwin") ?
             path.resolve(__dirname + "/service_declarations") :
-            path.resolve("/etc/systemd/user"),
+            path.resolve("/etc/systemd/system"),
         ...os.userInfo()
     }
 }
